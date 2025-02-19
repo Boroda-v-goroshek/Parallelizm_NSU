d4cf152b238ed0389afe9fcd41407747a0777d44
Boroda-V-Goroshek <Curobris@yandex.ru>
2025-02-19 09:19:08 +0700

    Update cmake-branch

---
+++ task1/build/CMakeFiles/double.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/double.dir/main.cpp.o"
+  "CMakeFiles/double.dir/main.cpp.o.d"
+  "double"
+  "double.pdb"
+  "float"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/double.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
